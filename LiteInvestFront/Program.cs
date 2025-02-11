a315a6893235eb112bbde684cd168a750ae4059a
FinAlgoTrader <algofinteh@gmail.com>
2025-02-11 21:39:07 +0400

    Ошибка в апи дата сервис Кестрел не перенес...

--- LiteInvestFront/Program.cs
+++ LiteInvestFront/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddSingleton<ApiDataService>();
+builder.Services.AddScoped<ApiDataService>();
 builder.Services.AddScoped<JsInteropService>();
 builder.Services.AddTelerikBlazor();
 
